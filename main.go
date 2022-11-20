1cb0f7d41955d6da5fd09460285980f9716b0df7
gemm123 <gemaakbar213@gmail.com>
2022-11-20 18:05:18 +0700

    feat: cors

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -28,10 +29,14 @@ func main() {
 	defer config.CloseDB()
 
 	r := gin.Default()
+
+	r.Use(cors.Default())
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
 	r.Run(":" + port)
 }
